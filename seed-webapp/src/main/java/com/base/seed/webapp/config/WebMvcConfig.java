41f6450f9dbf4b286faa018bec5182ffadbe8f51
zhangzheng <zhangzheng@itiger.com>
2020-06-13 22:09:13 +0800

    fix(login): remove non-exist depends

--- seed-webapp/src/main/java/com/base/seed/webapp/config/WebMvcConfig.java
+++ seed-webapp/src/main/java/com/base/seed/webapp/config/WebMvcConfig.java
@@ -3,22 +3,16 @@ package com.base.seed.webapp.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import com.base.seed.webapp.support.login.DefaultLoginAuthorizationInterceptor;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-  @Autowired
-  private DefaultLoginAuthorizationInterceptor defaultLoginAuthorizationInterceptor;
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -40,11 +34,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
     fastMediaTypes.add(MediaType.APPLICATION_JSON);
     converter.setSupportedMediaTypes(fastMediaTypes);
     converters.add(0, converter);
-  }
-
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(defaultLoginAuthorizationInterceptor)
-        .addPathPatterns("/**");
   }
 }
