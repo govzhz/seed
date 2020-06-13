0499bc60b538aa82aa5fdae24775e03ad43d4949
zhangzheng <zhangzheng@itiger.com>
2020-06-13 22:21:11 +0800

    chore(config): add gitignore

---
+++ seed-integration/src/main/java/com/base/seed/integration/client/DemoClient.java
@@ -0,0 +1,5 @@
+package com.base.seed.integration.client;
+
+public class DemoClient {
+
+}
---
+++ seed-integration/src/main/java/com/base/seed/integration/common/Demo.java
@@ -0,0 +1,8 @@
+package com.base.seed.integration.common;
+
+/**
+ * @author zhangzheng 2020/6/13
+ */
+public class Demo {
+
+}
